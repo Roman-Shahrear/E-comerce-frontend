d2be6438452aa0ff315fc9f06814e943440f5cb1
Roman-Shahrear <roman.shahrear@gmail.com>
2024-01-29 18:24:24 +0600

    adding functionality add to card button and add in cart also in cart state

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,10 +1,38 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = ()=> {
+  let cart = {};
+  for(let index= 0; index < all_product.length + 1; index++){
+    cart[index] = 0;
+  }
+  return cart;
+}
+
 const ShopContextProvider = (props) => {
-  const contextValue = {all_product};
+  
+  const [cartItems, setCartItem] = useState(getDefaultCart());
+  
+  const addToCart = (itemId) => {
+    setCartItem((prev) => {
+      const updatedCart = { ...prev, [itemId]: prev[itemId] + 1 };
+      return updatedCart;
+    });
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItem((prev) => {
+      const updatedCart = { ...prev, [itemId]: prev[itemId] + 1 };
+      return updatedCart;
+    });
+  };
+
+  useEffect(() => {
+  }, [cartItems]);
+
+  const contextValue = {all_product, cartItems, addToCart, removeFromCart};
 
   return (
     <ShopContext.Provider value={contextValue}>
